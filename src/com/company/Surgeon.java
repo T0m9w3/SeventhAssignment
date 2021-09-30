f0f7168d2054b95765dbb2709216504c4c05eeb5
trourke <trourke@student.bridgew.edu>
2021-09-30 11:42:20 -0400

    Initial Commit of Updated Version

--- src/com/company/Surgeon.java
+++ src/com/company/Surgeon.java
@@ -1,4 +1,17 @@
 package com.company;
 
+import java.util.Random;
+
 public class Surgeon extends Doctor{
+    private String Hospital;
+
+    public Surgeon(String hospital){
+       super(999.99, "Fancy Name Brand School");
+       Hospital = hospital;
+    }
+    public void treatPatient(Person Patient){
+        var hoursPicker = new Random();
+        System.out.println("After "+ hoursPicker.nextInt(8) + " hours of surgery at"+ Hospital + " our patient should be on the mend");
+        Patient.getTreated();
+    }
 }
